c49770f176a6141147c62458f6d6a8692f65c393
devopscomputer <pauloti.engenharia@gmail.com>
2026-04-24 13:37:12 -0300

    chore: update src/mev/market_truth/mod.rs

--- src/mev/market_truth/mod.rs
+++ src/mev/market_truth/mod.rs
@@ -2,5 +2,6 @@ pub mod competition_reality;
 pub mod edge_survival;
 pub mod execution_outcome_real;
 pub mod execution_replay_engine;
+pub mod market_snapshot_engine;
 pub mod markout_engine;
 pub mod truth_pipeline;
